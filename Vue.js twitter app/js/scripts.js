75a70d58d6ae724655fab07a6df62849934ffc81
deidrah <dominika@iMac-Dominika.local>
2017-05-10 13:22:07 +0200

    adding number of characters remaining

--- Vue.js twitter app/js/scripts.js
+++ Vue.js twitter app/js/scripts.js
@@ -1,11 +1,17 @@
+const MAX_TWEET_LENGTH = 140;
+
 new Vue({
     el: '#twitterVue',
     data: {
-    tweet: ''
+        tweet: ''
     },
     computed: {
-        tweetIsEmpty: function() {
-            return this.tweet.length == 0;
+        tweetIsOutOfRange: function () {
+            return this.charactersRemaining == MAX_TWEET_LENGTH ||
+                this.charactersRemaining < 0;
+        },
+        charactersRemaining: function () {
+            return MAX_TWEET_LENGTH - this.tweet.length;
         }
     }
 });
